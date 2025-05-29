68c00d3fe007b18c47503b2538a798e130b413d5
Timofey <shabalin.timon@gmail.com>
2025-05-29 21:32:25 +0300

    Добавил роли (админ, клиент, курьер) и сделал миграцию с обновлением бд

--- FoodDesk.Persistence/Context/ApplicationDbContext.cs
+++ FoodDesk.Persistence/Context/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 ﻿ using FoodDesk.Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -12,5 +13,29 @@ public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
+
+        var admin = new IdentityRole
+        {
+            Id = "1",
+            Name = "admin",
+            NormalizedName = "ADMIN"
+        };
+
+        var client = new IdentityRole
+        {
+            Id = "2",
+            Name = "client",
+            NormalizedName = "CLIENT"
+        };
+
+        var courier = new IdentityRole
+        {
+            Id = "3",
+            Name = "courier",
+            NormalizedName = "COURIER"
+        };
+
+        modelBuilder.Entity<IdentityRole>().HasData(admin, client, courier);
     }
+
 }
