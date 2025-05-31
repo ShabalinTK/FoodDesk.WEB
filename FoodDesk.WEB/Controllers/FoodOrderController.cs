b1777b5215b6e48289944cb333d30678b6eb5366
Timofey <shabalin.timon@gmail.com>
2025-05-31 08:53:01 +0300

    Добавил страницы и контроллеры для Client

---
+++ FoodDesk.WEB/Controllers/FoodOrderController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FoodDesk.WEB.Controllers
+{
+    public class FoodOrderController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
