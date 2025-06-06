e406007d4c6ee866567ca583e83f2d7319ded867
Timofey <108810276+ShabalinTK@users.noreply.github.com>
2025-06-06 12:39:59 +0300

    Обновил таблицы в бд сделал миграцию обновил таблицу

--- FoodDesk.Domain/Entities/Order.cs
+++ FoodDesk.Domain/Entities/Order.cs
@@ -3,11 +3,12 @@
 public class Order
 {
     public int Id { get; set; }
-    public int UserId { get; set; } // Связь с пользователем
+    public int UserProfileId { get; set; }
+    public UserProfile UserProfile { get; set; }
     public string RestaurantName { get; set; }
-    public int DeliveryTime { get; set; } // В минутах
-    public decimal Distance { get; set; } // В км
-    public string Status { get; set; } // Completed, Delivering, Preparing
+    public int DeliveryTime { get; set; }
+    public decimal Distance { get; set; }
+    public string Status { get; set; }
     public decimal TotalAmount { get; set; }
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     public List<OrderItem> OrderItems { get; set; }
---
+++ FoodDesk.Persistence/Migrations/20250606092809_newUserProfileAndOrder.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace FoodDesk.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class newUserProfileAndOrder : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UserId",
+                table: "Orders",
+                newName: "UserProfileId");
+
+            migrationBuilder.CreateTable(
+                name: "UserProfile",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    FullName = table.Column<string>(type: "text", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "text", nullable: false),
+                    Address = table.Column<string>(type: "text", nullable: false),
+                    IsCourier = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserProfile", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_UserProfileId",
+                table: "Orders",
+                column: "UserProfileId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_UserProfile_UserProfileId",
+                table: "Orders",
+                column: "UserProfileId",
+                principalTable: "UserProfile",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_UserProfile_UserProfileId",
+                table: "Orders");
+
+            migrationBuilder.DropTable(
+                name: "UserProfile");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Orders_UserProfileId",
+                table: "Orders");
+
+            migrationBuilder.RenameColumn(
+                name: "UserProfileId",
+                table: "Orders",
+                newName: "UserId");
+        }
+    }
+}
